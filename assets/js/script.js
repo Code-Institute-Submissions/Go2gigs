76018e629531c18e41a434ee698329a11be2ed04
Conal84 <conal.walsh1@gmail.com>
2020-01-18 13:43:36 +0000

    Script.js began work on geocoding an address

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
+
+    // save user input to a variable when submitted
     $("#gigForm").submit(function (event) {
         var userInput = $("#gigInput").val();
-        console.log(userInput);
-        alert("input is:" + userInput);
+
+        var geocoder = new google.maps.Geocoder();
+
+        geocoder.geocode( {'address': userInput}, function(results, status){
+
+            if (status == google.maps.GeocoderStatus.OK){
+                var latitude = results[0].geometry.location.lat();
+                var longitude = results[0].geometry.location.lng();
+                alert("Lat is;" + latitude);
+            }
+        });
     });
 });

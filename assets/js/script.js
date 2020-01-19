f3ed2441de2867154b5eeac120e70f8711eeaaa8
Conal84 <conal.walsh1@gmail.com>
2020-01-19 19:21:09 +0000

    Script.js added geocode function and modified .submit function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     // save user input to a variable when submitted
     $("#gigForm").submit(function (event) {
-        var userInput = $("#gigInput").val();
+        var userInput = String($("#gigInput").val());
+        console.log(userInput);
+        event.preventDefault();
 
-        var geocoder = new google.maps.Geocoder();
+        geocode(userInput);
 
-        geocoder.geocode( {'address': userInput}, function(results, status){
-
-            if (status == google.maps.GeocoderStatus.OK){
-                var latitude = results[0].geometry.location.lat();
-                var longitude = results[0].geometry.location.lng();
-                alert("Lat is;" + latitude);
-            }
-        });
     });
+
+    function geocode(location){
+        axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+            params: {
+                address:location,
+                key:'AIzaSyCEpPkjqOvCqa0kghMNInbffbMj8STkhB4'
+            }
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    }
 });

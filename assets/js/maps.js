e63ec79adeefb8a8d3a86cf9c30aca9ee705267c
Conal84 <conal.walsh1@gmail.com>
2020-01-21 19:47:27 +0000

    Added Display point function to script.js which sets a new google maps marker at the user input location

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,11 +1,17 @@
 
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
-  var location = {lat: 51.5095, lng: -0.1245};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: location});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: location, map: map});
+    // The location of Uluru
+    // var location = { lat: latitude, lng: longitude };
+    // The map, centered at 0,0
+    var map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 4, center: { lat: 0, lng: 0 } });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.drop,
+        position: { lat: 0, lng: 0 }
+    });
 }
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,53 @@
-$(document).ready(function () {
 
-    // save user input to a variable when submitted
-    $("#gigForm").submit(function (event) {
-        var userInput = String($("#gigInput").val());
-        console.log(userInput);
-        event.preventDefault();
+    // Initialize and add the map
+    function initMap() {
+        // The map, centered at 0,0
+        var map = new google.maps.Map(
+            document.getElementById('map'), { zoom: 4, center: { lat: 0, lng: 0 } });
+        // The marker, positioned at 0,0
+        var marker = new google.maps.Marker({
+            map: map,
+            draggable: true,
+            animation: google.maps.Animation.drop,
+            position: { lat: 0, lng: 0 }
+        });
 
-        geocode(userInput);
+        // save user input to a variable when submitted
+        $("#gigForm").submit(function (event) {
+            var userInput = String($("#gigInput").val());
+            event.preventDefault();
+            // call geocode function with userInput
+            geocode(userInput);
+        });
 
-    });
+        // Geocode the user input location into latitude and longitude using google geocode api
+        function geocode(location) {
+            axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+                params: {
+                    address: location,
+                    key: 'AIzaSyCEpPkjqOvCqa0kghMNInbffbMj8STkhB4'
+                }
+            })
+            .then(function (response) {
+                var latitude = parseFloat(response.data.results[0].geometry.location.lat);
+                var longitude = parseFloat(response.data.results[0].geometry.location.lng);
+                console.log(latitude);
+                console.log(longitude);
+                DisplayPoint(map, latitude, longitude);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        }
 
-    function geocode(location){
-        axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-            params: {
-                address:location,
-                key:'AIzaSyCEpPkjqOvCqa0kghMNInbffbMj8STkhB4'
-            }
-        })
-        .then(function (response) {
-            var latitude = parseFloat(response.data.results[0].geometry.location.lat);
-            var longitude = parseFloat(response.data.results[0].geometry.location.lng);
-            console.log(latitude);
-            console.log(longitude);
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
+        function DisplayPoint(map, latitude, longitude){
+
+            var latLng = new google.maps.LatLng(latitude, longitude);
+            marker.setPosition(latLng);
+            map.panTo(latLng);
+
+        }
     }
-});
+
+
+    

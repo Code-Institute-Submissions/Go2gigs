e1611b93a6a12af31020d37a54b42eeae28c4f34
Conal84 <conal.walsh1@gmail.com>
2020-02-01 14:17:41 +0000

    Changed getData function to prevent undefined error

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,21 @@ $( document ).ready(function(){
     function getData(response) {
         var total = parseInt(response.data.resultsPage.totalEntries);
         console.log(total);
+
+        var second = response.data.resultsPage.results.event[1].displayName
+        console.log(second);
+
+        var data = [];
+        for(var i = 0; i < total; i++){
+            data.push({
+                'Artist': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].displayName,
+                'Date': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].start && response.data.resultsPage.results.event[i].start.date,
+                'City': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].location && response.data.resultsPage.results.event[i].location.city,
+                'Venue': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].venue && response.data.resultsPage.results.event[i].venue.displayName
+            });
+        }
+        console.log(data);
+        return data
     }
 
     // $(function() {

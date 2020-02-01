11b0a7074031f55e10d76f52b546c58d158089df
Conal84 <conal.walsh1@gmail.com>
2020-02-01 12:15:25 +0000

    Changed html search button, modified script.js submit event and got the findEvent function to return a songkick object

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,17 +1,40 @@
 
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    // var location = { lat: latitude, lng: longitude };
-    // The map, centered at 0,0
-    var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 4, center: { lat: 0, lng: 0 } });
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({
-        map: map,
-        draggable: true,
-        animation: google.maps.Animation.drop,
-        position: { lat: 0, lng: 0 }
-    });
-}
+// Initialize and add the map, centre it and add a marker at the default position of central europe
+// function initMap() {
+//     // The map, centered at central europe lat and long
+//     var map = new google.maps.Map(
+//         document.getElementById('map'), { zoom: 8, center: { lat: 50.3785, lng: 14.9706 } });
+//     // The marker, positioned at central europe lat and long
+//     var marker = new google.maps.Marker({
+//         map: map,
+//         draggable: true,
+//         animation: google.maps.Animation.drop,
+//         position: { lat: 50.3785, lng: 14.9706 }
+//     });
 
+        // Geocode the user input location into latitude and longitude using google geocode api
+    // function geocode(location) {
+    //     axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+    //         params: {
+    //             address: location,
+    //             key: 'AIzaSyCEpPkjqOvCqa0kghMNInbffbMj8STkhB4'
+    //         }
+    //     })
+    //         .then(function (response) {
+    //             var latitude = parseFloat(response.data.results[0].geometry.location.lat);
+    //             var longitude = parseFloat(response.data.results[0].geometry.location.lng);
+    //             // call DisplayPoint function with lat and long
+    //             console.log(latitude);
+    //             console.log(longitude);
+    //             DisplayPoint(map, latitude, longitude);
+    //         })
+    //         .catch(function (error) {
+    //             console.log(error);
+    //         })
+    // }
+
+    // function DisplayPoint(map, latitude, longitude) {
+    //     var latLng = new google.maps.LatLng(latitude, longitude);
+    //     marker.setPosition(latLng);
+    //     map.panTo(latLng);
+    // }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,14 @@
+$( document ).ready(function(){
 
-// Initialize and add the map, centre it and add a marker at the default position of central europe
-function initMap() {
-    // The map, centered at central europe lat and long
-    var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 8, center: { lat: 50.3785, lng: 14.9706 } });
-    // The marker, positioned at central europe lat and long
-    var marker = new google.maps.Marker({
-        map: map,
-        draggable: true,
-        animation: google.maps.Animation.drop,
-        position: { lat: 50.3785, lng: 14.9706 }
-    });
-
-    // When a user submits input save that input to a variable and call the FindSkId function to find a songkick id
-    $("#gigForm").submit(function (event) {
-        var userInput = String($("#gigInput").val());
+// When a user submits input save that input to a variable and call the findEvents function
+    $("#search-form").submit(function(event){
+        var userInput = String($("#user-input").val());
+        console.log(userInput);
         event.preventDefault();
-        // call geocode function with userInput
-        // geocode(userInput);
         findEvents(userInput);
     });
 
-    // Geocode the user input location into latitude and longitude using google geocode api
-    function geocode(location) {
-        axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-            params: {
-                address: location,
-                key: 'AIzaSyCEpPkjqOvCqa0kghMNInbffbMj8STkhB4'
-            }
-        })
-            .then(function (response) {
-                var latitude = parseFloat(response.data.results[0].geometry.location.lat);
-                var longitude = parseFloat(response.data.results[0].geometry.location.lng);
-                // call DisplayPoint function with lat and long
-                console.log(latitude);
-                console.log(longitude);
-                DisplayPoint(map, latitude, longitude);
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-    }
-
-    function DisplayPoint(map, latitude, longitude) {
-        var latLng = new google.maps.LatLng(latitude, longitude);
-        marker.setPosition(latLng);
-        map.panTo(latLng);
-    }
-
+    // FindEvents takes user artist input passes that and apikey to songkick api and obtains a response
     function findEvents(userInput) {
         axios.get('https://api.songkick.com/api/3.0/events.json', {
             params: {
@@ -55,8 +16,8 @@ function initMap() {
                 artist_name: userInput
             }
         })
-            .then(function (response2) {
-                console.log(response2);
+            .then(function (response) {
+                console.log(response);
                 // tabulate(response2);
             })
             .catch(function (error) {
@@ -69,6 +30,5 @@ function initMap() {
     //         data: data
     //     });
     // });
-}
 
-
+});

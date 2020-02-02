dda3a3092f6587cbb958fd166e614efb660da156
Conal84 <conal.walsh1@gmail.com>
2020-02-02 00:24:28 +0000

    Attempted to get bootstrap table working

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,37 +21,63 @@ $( document ).ready(function(){
                 $(function() {
                     $('#table').bootstrapTable({data: getData(response)})
                 })
-
             })
             .catch(function (error) {
                 console.log(error);
             })
     }
 
-    function getData(response) {
-        var total = parseInt(response.data.resultsPage.totalEntries);
-        console.log(total);
+    // function getData(response) {
+    //     var total = parseInt(response.data.resultsPage.totalEntries);
+    //     console.log(total);
 
-        var second = response.data.resultsPage.results.event[1].displayName
-        console.log(second);
+    //     var second = response.data.resultsPage.results.event[1].displayName
+    //     console.log(second);
 
-        var data = [];
-        for(var i = 0; i < total; i++){
-            data.push({
-                'Artist': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].displayName,
-                'Date': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].start && response.data.resultsPage.results.event[i].start.date,
-                'City': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].location && response.data.resultsPage.results.event[i].location.city,
-                'Venue': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].venue && response.data.resultsPage.results.event[i].venue.displayName
-            });
+    //     var data = [];
+    //     for(var i = 0; i < total; i++){
+    //         data.push({
+    //             'Artist': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].displayName,
+    //             'Date': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].start && response.data.resultsPage.results.event[i].start.date,
+    //             'City': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].location && response.data.resultsPage.results.event[i].location.city,
+    //             'Venue': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].venue && response.data.resultsPage.results.event[i].venue.displayName
+    //         });
+    //     }
+    //     console.log(data);
+    //     return data;
+    // }
+    var $table = $('#table')
+
+    $(function() {
+        var data = [
+        {
+            'Artist': 'first',
+            'Date': '1/1/20',
+            'City': 'Man',
+            'Venue': 'Man1'
+        },
+        {
+            'Artist': 'second',
+            'Date': '2/2/20',
+            'City': 'Manc',
+            'Venue': 'Man2'
+        },
+        {
+            'Artist': 'third',
+            'Date': '3/3/20',
+            'City': 'Manch',
+            'Venue': 'Man3'
+        },
+        {
+            'Artist': 'fourth',
+            'Date': '4/4/20',
+            'City': 'Manche',
+            'Venue': 'Man4'
         }
-        console.log(data);
-        return data;
-    }
-
-    // $(function() {
-    //     $('#table').bootstrapTable({
-    //         data: data
-    //     });
-    // });
+        ]
+        $table.bootstrapTable({
+            data: data
+        });
+    });
 
 });

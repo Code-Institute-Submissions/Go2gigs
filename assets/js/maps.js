fe019f7ccd892578cf100688aea3019192a98436
Conal84 <conal.walsh1@gmail.com>
2020-02-04 19:52:31 +0000

    Added seperate function displayMarkers to set marker locations on map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,17 +1,26 @@
 
 // Initialize and add the map, centre it and add a marker at the default position of central europe
+var map;
+
 function initMap() {
     // The map, centered at central europe lat and long
-    var map = new google.maps.Map(
+    map = new google.maps.Map(
         document.getElementById('map'), { zoom: 8, center: { lat: 50.3785, lng: 14.9706 } });
-    // The marker, positioned at central europe lat and long
-    var marker = new google.maps.Marker({
-        map: map,
-        draggable: true,
-        animation: google.maps.Animation.drop,
-        position: { lat: 50.3785, lng: 14.9706 }
-    });
 }
+
+// function displayMarkers(){
+//     // Add markers for each location with numeric labels
+//     var markers = locations.map(function(location, i) {
+//           return new google.maps.Marker({
+//             position: location,
+//             label: labels[i % labels.length]
+//           });
+//         });
+
+//     // Add a marker clusterer to manage the markers.
+//     var markerCluster = new MarkerClusterer(map, markers,
+//         {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+// }
 
     // DisplayPoint(map, latitude, longitude);
 

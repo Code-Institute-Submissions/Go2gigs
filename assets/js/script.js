ea38c76922aa0ad1db761c44e8b2240bf014c9b2
Conal84 <conal.walsh1@gmail.com>
2020-02-08 08:55:10 +0000

    Script.js added function findLocation to get location id response from Songkick API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 $( document ).ready(function(){
+
     // When the user changes the select option change the user input placeholder
     $('#search-by').change(function(){       
         if($(this).val() == '0'){ // search by city
@@ -14,16 +15,35 @@ $( document ).ready(function(){
             var userInput = String($("#user-input").val());
             var dateFrom = $("#date-from").val();
             var dateTo = $("#date-to").val();
-            findEvents(userInput, dateFrom, dateTo);
+            findLocation(userInput);
             event.preventDefault();
         }else if($('#search-by').val() == '1'){ // search by artist
             var userInput = String($("#user-input").val());
             var dateFrom = $("#date-from").val();
             var dateTo = $("#date-to").val();
-            // findEvents(userInput, dateFrom, dateTo); new function here to find by location
+            findEvents(userInput, dateFrom, dateTo);
             event.preventDefault();
         }
     });
+
+    // FindLocation takes user location input passes that and apikey to songkick api and obtains a response
+    function findLocation(userInput) {
+        axios.get('https://api.songkick.com/api/3.0/search/locations.json?', {
+            params: {
+                query: userInput,
+                apikey: 'bguT074ohahXwEwu'
+            }
+        })
+            .then(function (response) {
+                $(function() {
+                    var id = response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.location[0] && response.data.resultsPage.results.location[0].metroArea && response.data.resultsPage.results.location[0].metroArea.id
+                    console.log(id);
+                })
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
 
     // FindEvents takes user artist input passes that and apikey to songkick api and obtains a response
     function findEvents(userInput, dateFrom, dateTo) {
@@ -50,6 +70,45 @@ $( document ).ready(function(){
             .catch(function (error) {
                 console.log(error);
             })
+    }
+
+    // getLocData takes the response, loops thru the response and push the required response data into array containers
+    function getLocData(response) {
+        var total = parseInt(response.data.resultsPage.totalEntries);
+        console.log(total); // remove this********
+
+        var labels = '';
+        var locations = [];
+        var data = [];
+
+        for(var i = 0; i < total; i++){
+            data.push({
+                '#': i+1,
+                'Artist': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].displayName,
+                'Date': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].start && response.data.resultsPage.results.event[i].start.date,
+                'City': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].location && response.data.resultsPage.results.event[i].location.city,
+                'Venue': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].venue && response.data.resultsPage.results.event[i].venue.displayName,
+                'Lat': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].location && response.data.resultsPage.results.event[i].location.lat,
+                'Lng': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].location && response.data.resultsPage.results.event[i].location.lng
+            });
+
+            labels = labels + i.toString();
+
+            locations.push({
+                'lat': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].location && response.data.resultsPage.results.event[i].location.lat,
+                'lng': response.data && response.data.resultsPage && response.data.resultsPage.results && response.data.resultsPage.results.event[i] && response.data.resultsPage.results.event[i].location && response.data.resultsPage.results.event[i].location.lng
+            })
+        }
+        console.log(data); // remove this**********
+        console.log(labels);  // remove this**********
+        console.log(locations);  // remove this**********
+
+        // The function returns an object of all array containers
+        return {
+            tableData: data,
+            labelData: labels,
+            locationData: locations
+        };
     }
 
     // getData takes the response, loops thru the response and push the required response data into array containers
@@ -82,7 +141,6 @@ $( document ).ready(function(){
         console.log(data); // remove this**********
         console.log(labels);  // remove this**********
         console.log(locations);  // remove this**********
-        // displayMarkers(labels, locations);
 
         // The function returns an object of all array containers
         return {

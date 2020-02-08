ed7a567bcf53a7db7c29daf3c28ea6f28ab8ba3b
Conal84 <conal.walsh1@gmail.com>
2020-02-08 14:13:41 +0000

    Tried to get axios get api call working with nested array

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,10 +49,22 @@ $( document ).ready(function(){
     }
 
     function findEventLoc(locId, dateFrom, dateTo){
+        axios.interceptors.request.use(config => {
+            config.paramsSerializer = params => {
+                // Qs is already included in the Axios package
+                return Qs.stringify(params, {
+                    arrayFormat: "brackets",
+                    // encode: false
+                });
+            };
+            return config;
+        });
         axios.get('https://api.songkick.com/api/3.0/events.json', {
             params: {
                 apikey: 'bguT074ohahXwEwu',
-                location:locId,
+                location: {
+                    'sk':locId
+                },
                 min_date: dateFrom,
                 max_date: dateTo
             }
@@ -88,6 +100,7 @@ $( document ).ready(function(){
             .then(function (response) {
                 $(function() {
                     // The function getData is called here and saved to a variable
+                    console.log(response);
                     var myData = getData(response);
 
                     // The array data returned from function getData is tabulated using the bootstrap table function

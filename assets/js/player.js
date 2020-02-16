2b3012b86c09ed0a92ffd48ee884b8c19b19bfd3
Conal84 <conal.walsh1@gmail.com>
2020-02-16 19:42:08 +0000

    Added player.js and youtube video to html

---
+++ assets/js/player.js
@@ -0,0 +1,38 @@
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: 'Wq4tyDRhU_4',
+    });
+}
+
+
+
+function findPlaylist(searchTerm) {
+
+    axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+            part: searchTerm,
+            apikey: 'AIzaSyBM28Mpnwfy8kj3KF8QJF24LsnTMvgqR68'
+        }
+    })
+        .then(function (response) {
+            $(function () {
+                console.log(response);
+            })
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}

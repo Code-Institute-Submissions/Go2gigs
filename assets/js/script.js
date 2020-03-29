0de7f4bd9f02a6ea4c09d1432ea269a31a7afc1b
Conal84 <conal.walsh1@gmail.com>
2020-03-29 17:24:36 +0000

    Removed validation event listener windown addEventListener

--- assets/js/script.js
+++ assets/js/script.js
@@ -414,28 +414,28 @@ $(document).ready(function () {
             console.log('fetch failed', err);
         }
     }
-    (function () {
-        'use strict';
-        window.addEventListener('load', function () {
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            let forms = document.getElementsByClassName('needs-validation');
-            // Loop over them and prevent submission
-            let validation = Array.prototype.filter.call(forms, function (form) {
-                form.addEventListener('submit', function (event) {
-                    if (form.checkValidity() === false) {
-                        event.preventDefault();
-                        event.stopPropagation();
-                    }
-                    else if (form.checkValidity() === true) {
-                        search();
-                        form.classList.remove('was-validated');
-                    }
-                    form.classList.add('was-validated');
-                }, false);
-            });
-        }, false);
-    })();
-});
+    // (function () {
+    //     'use strict';
+    // window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        let forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        let validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                else if (form.checkValidity() === true) {
+                    search();
+                    form.classList.remove('was-validated');
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    // }, false);
+})
+// });
 
 
 
